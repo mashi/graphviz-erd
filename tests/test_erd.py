8d7027d1050a2e59e3d24764d073463283695b15
mashi <59000162+mashi@users.noreply.github.com>
2022-02-28 17:30:05 -0300

    test strings from different staticmethods

--- tests/test_erd.py
+++ tests/test_erd.py
@@ -1,9 +1,36 @@
 import unittest
 
+from src.erd import ERD
+
 
 class TestErd(unittest.TestCase):
-    def test_passing(self):
+    def test_passing_test(self):
         self.assertEqual(1, 1)
+
+    def test_process_label_none(self):
+        answer = None
+        calculated = ERD._process_label(None)
+        self.assertEqual(calculated, answer)
+
+    def test_process_label_str(self):
+        answer = "<a>"
+        calculated = ERD._process_label("a")
+        self.assertEqual(calculated, answer)
+
+    def test_str_key(self):
+        answer = "<<u>a</u>>"
+        calculated = ERD._str_key("a")
+        self.assertEqual(calculated, answer)
+
+    def test_weak_key(self):
+        answer = """<<TABLE BORDER="0" CELLBORDER="1" CELLSPACING="0" CELLPADDING="0">
+    <TR>
+        <TD COLSPAN="1" SIDES="B" STYLE="dashed">a</TD>
+    </TR>
+    </TABLE>>"""
+        calculated = ERD._weak_key("a")
+        print(calculated)
+        self.assertEqual(calculated, answer)
 
 
 if __name__ == "__main__":
