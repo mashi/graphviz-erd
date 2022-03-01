89357ded85d27ffdda412bb5f4b450be29af9a70
mashi <59000162+mashi@users.noreply.github.com>
2022-02-28 21:08:47 -0300

    Tests (#6)

--- src/erd.py
+++ src/erd.py
@@ -7,8 +7,12 @@ class ERD:
     """
     Parameters
     ----------
-    gr
-        A object from the graphviz.Graph class.
+    filename
+        Name of the generated dot code and image file.
+    engine
+        Specify the dot engine used to generate the image.
+    image_format
+        The image extension.
 
     Attributes
     ----------
@@ -16,8 +20,16 @@ class ERD:
         Graphviz object.
     """
 
-    def __init__(self, gr: graphviz.Graph) -> None:
-        self.gr = gr
+    def __init__(
+        self,
+        filename: str = "erd",
+        engine: str = "dot",
+        image_format: str = "png",
+    ) -> None:
+        print(format)
+        self.gr = graphviz.Graph(
+            "ER", filename=filename, engine=engine, format=image_format
+        )
 
     @staticmethod
     def _process_label(text: Optional[str]) -> Optional[str]:
@@ -45,7 +57,6 @@ class ERD:
         ----------
         node_name
             Name of the node.
-
         node_label
             Name that will appear at the node.
         """
@@ -58,7 +69,6 @@ class ERD:
         ----------
         node_name
             Name of the node.
-
         node_label
             Name that will appear at the node.
         """
@@ -78,7 +88,6 @@ class ERD:
         ----------
         node_name
             Name of the node.
-
         node_label
             Name that will appear at the node.
         """
@@ -94,7 +103,6 @@ class ERD:
         ----------
         node_name
             Name of the node.
-
         node_label
             Name that will appear at the node.
         """
@@ -109,7 +117,6 @@ class ERD:
         ----------
         node_name
             Name of the node.
-
         node_label
             Name that will appear at the node.
         """
@@ -121,6 +128,31 @@ class ERD:
             peripheries="2",
         )
 
+    @staticmethod
+    def _check_node_label(
+        node_name: str, node_label: Optional[str] = None
+    ) -> str:
+        """Returns the node_name if the node_label is None. Otherwise, the
+        node_name will be used as the label for the graphviz node.
+
+        Parameters
+        ----------
+        node_name
+            Name of the node.
+        node_label
+            Name that will appear at the node.
+
+        Returns
+        -------
+        str
+            The string that will be used as label.
+        """
+        if node_label is None:
+            text = node_name
+        else:
+            text = node_label
+        return text
+
     def key(self, node_name: str, node_label: Optional[str] = None):
         """This method declares a key attribute.
 
@@ -128,14 +160,10 @@ class ERD:
         ----------
         node_name
             Name of the node.
-
         node_label
             Name that will appear at the node.
         """
-        if node_label is None:
-            text = node_name
-        else:
-            text = node_label
+        text = self._check_node_label(node_name, node_label)
         self.gr.node(node_name, self._str_key(text), shape="ellipse")
 
     @staticmethod
@@ -145,13 +173,47 @@ class ERD:
         Parameters
         ----------
         label
-            Label to processed.
+            Label to be processed.
 
         Returns
         -------
-        Processed label.
+        str
+            Processed label.
         """
         return f"<<u>{label}</u>>"
+
+    def weak_key(self, node_name: str, node_label: Optional[str] = None):
+        """This method declares a weak key attribute.
+
+        Parameters
+        ----------
+        node_name
+            Name of the node.
+        node_label
+            Name that will appear at the node.
+        """
+        text = self._check_node_label(node_name, node_label)
+        self.gr.node(node_name, label=self._str_weak_key(text), shape="ellipse")
+
+    @staticmethod
+    def _str_weak_key(label: str) -> str:
+        """String processing for weak key attributes.
+
+        Parameters
+        ----------
+        label
+            Label to be processed.
+
+        Returns
+        -------
+        str
+            Processed label.
+        """
+        return f"""<<TABLE BORDER="0" CELLBORDER="1" CELLSPACING="0" CELLPADDING="0">
+    <TR>
+        <TD COLSPAN="1" SIDES="B" STYLE="dashed">{label}</TD>
+    </TR>
+    </TABLE>>"""
 
     def derived(self, node_name: str, node_label: Optional[str] = None):
         """This method declares a derived attribute.
@@ -160,7 +222,6 @@ class ERD:
         ----------
         node_name
             Name of the node.
-
         node_label
             Name that will appear at the node.
         """
@@ -170,42 +231,6 @@ class ERD:
             shape="ellipse",
             style="dashed",
         )
-
-    def weak_key(self, node_name: str, node_label: Optional[str] = None):
-        """This method declares a weak key attribute.
-
-        Parameters
-        ----------
-        node_name
-            Name of the node.
-
-        node_label
-            Name that will appear at the node.
-        """
-        if node_label is None:
-            text = node_name
-        else:
-            text = node_label
-        self.gr.node(node_name, label=self._weak_key(text), shape="ellipse")
-
-    @staticmethod
-    def _weak_key(label: str) -> str:
-        """String processing for weak key attributes.
-
-        Parameters
-        ----------
-        label
-            Label to processed.
-
-        Returns
-        -------
-        Processed label.
-        """
-        return f"""<<TABLE BORDER="0" CELLBORDER="1" CELLSPACING="0" CELLPADDING="0">
-    <TR>
-        <TD COLSPAN="1" SIDES="B" STYLE="dashed">{label}</TD>
-    </TR>
-    </TABLE>>"""
 
     def relation(
         self,
@@ -244,8 +269,7 @@ class ERD:
 
 
 if __name__ == "__main__":
-    gr = graphviz.Graph("ER", filename="erd", engine="dot", format="png")
-    obj = ERD(gr)
+    obj = ERD()
 
     # node name and node label
     obj.entity("entity1", "entity 1")
--- tests/test_erd.py
+++ tests/test_erd.py
@@ -1,9 +1,142 @@
 import unittest
 
+from src.erd import ERD
+
 
 class TestErd(unittest.TestCase):
-    def test_passing(self):
+    def test_passing_test(self):
         self.assertEqual(1, 1)
+
+    def test_process_label_none(self):
+        answer = None
+        calculated = ERD._process_label(None)
+        self.assertEqual(calculated, answer)
+
+    def test_process_label_str(self):
+        answer = "<a>"
+        calculated = ERD._process_label("a")
+        self.assertEqual(calculated, answer)
+
+    def test_str_key(self):
+        answer = "<<u>a</u>>"
+        calculated = ERD._str_key("a")
+        self.assertEqual(calculated, answer)
+
+    def test_str_weak_key(self):
+        answer = """<<TABLE BORDER="0" CELLBORDER="1" CELLSPACING="0" CELLPADDING="0">
+    <TR>
+        <TD COLSPAN="1" SIDES="B" STYLE="dashed">a</TD>
+    </TR>
+    </TABLE>>"""
+        calculated = ERD._str_weak_key("a")
+        print(calculated)
+        self.assertEqual(calculated, answer)
+
+    def test_check_node_label_none(self):
+        node_name = "nodename"
+        node_label = None
+        answer = node_name
+        calculated = ERD._check_node_label(node_name, node_label)
+        self.assertEqual(calculated, answer)
+
+    def test_check_node_label_str(self):
+        node_name = "nodename"
+        node_label = "node label"
+        answer = node_label
+        calculated = ERD._check_node_label(node_name, node_label)
+        self.assertEqual(calculated, answer)
+
+    def test_entity(self):
+        obj = ERD()
+        name = "entity1"
+        obj.entity(name)
+        calculated = obj.gr.source
+        answer = f"{name} [shape=box]"
+        self.assertIn(answer, calculated)
+
+    def test_weak_entity(self):
+        obj = ERD()
+        name = "entity1"
+        obj.weak_entity(name)
+        calculated = obj.gr.source
+        answer = f"{name} [peripheries=2 shape=box]"
+        self.assertIn(answer, calculated)
+
+    def test_associative_entity(self):
+        obj = ERD()
+        name = "entity1"
+        obj.associative_entity(name)
+        calculated = obj.gr.source
+        answer = f"{name} [shape=Msquare]"
+        self.assertIn(answer, calculated)
+
+    def test_attribute(self):
+        obj = ERD()
+        name = "attribute"
+        obj.attribute(name)
+        calculated = obj.gr.source
+        answer = f"{name} [shape=ellipse]"
+        self.assertIn(answer, calculated)
+
+    def test_multivalue(self):
+        obj = ERD()
+        name = "multivalued"
+        obj.multivalue(name)
+        calculated = obj.gr.source
+        answer = f"{name} [peripheries=2 shape=ellipse]"
+        self.assertIn(answer, calculated)
+
+    def test_key(self):
+        obj = ERD()
+        name = "key"
+        obj.key(name)
+        calculated = obj.gr.source
+        answer_shape = "shape=ellipse"
+        answer_label = ERD._str_key(name)
+        self.assertIn(answer_shape, calculated)
+        self.assertIn(answer_label, calculated)
+
+    def test_weak_key(self):
+        obj = ERD()
+        name = "key"
+        obj.weak_key(name)
+        calculated = obj.gr.source
+        answer_shape = "shape=ellipse"
+        answer_label = ERD._str_weak_key(name)
+        self.assertIn(answer_shape, calculated)
+        print("answer")
+        print(answer_label)
+        print("calculated")
+        print(calculated)
+        self.assertIn(answer_label, calculated)
+
+    def test_derived(self):
+        obj = ERD()
+        name = "key"
+        obj.derived(name)
+        calculated = obj.gr.source
+        answer = "[shape=ellipse style=dashed]"
+        self.assertIn(answer, calculated)
+
+    def test_relation(self):
+        obj = ERD()
+        name = "relationship"
+        obj.relation(name, "ent1", "ent2")
+        calculated = obj.gr.source
+        answer_shape = "shape=diamond"
+        answer_connection1 = f"ent1 -- {name}"
+        answer_connection2 = f"ent2 -- {name}"
+        self.assertIn(answer_shape, calculated)
+        self.assertIn(answer_connection1, calculated)
+        self.assertIn(answer_connection2, calculated)
+
+    def test_relation_identifying(self):
+        obj = ERD()
+        name = "relationship"
+        obj.relation(name, "ent1", "ent2", identifying="yes")
+        answer = "peripheries=2"
+        calculated = obj.gr.source
+        self.assertIn(answer, calculated)
 
 
 if __name__ == "__main__":
